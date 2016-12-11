3dcb47efc12c5856e5c1645786f9c774e3751316
Tess Elation <fredisded@gmail.com>
2016-12-11 22:17:56 +1100

    working prototype of movies I

--- Tess/wk4/movies/app.rb
+++ Tess/wk4/movies/app.rb
@@ -11,22 +11,20 @@ def get_movie(movie)
 end
 
 
-get "/" do
-  erb :index
-end
 
-# get "/" do
-#
-#   @movie = params[:movie]
-#     if @movie
-#     @movie_raw = get_stock_quote @movie_raw
-#     @title = @movie_raw["Title"]
-#     @year = @movie_raw["Year"]
-#     @director = @movie_raw["Director"]
-#     @runtime = @movie_raw["Runtime"]
-#   end
-#
-#
-#   erb :index
-#
-# end
+get "/" do
+
+  @movie = params[:movie]
+    if @movie
+    @movie_raw = get_movie @movie
+    @title = @movie_raw["Title"]
+    @year = @movie_raw["Year"]
+    @director = @movie_raw["Director"]
+    @runtime = @movie_raw["Runtime"]
+    @poster = @movie_raw["Poster"]
+  end
+
+
+  erb :index
+
+end
